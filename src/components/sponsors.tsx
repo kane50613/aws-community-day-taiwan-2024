422e9437160ea506c3bdd57d1371286f27b3d48f
Franky Chen <franky@arcloud.com.tw>
2024-07-26 05:37:09 -0400

    feat: add sponsor link

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -4,27 +4,32 @@ import { BlurIn } from "@/components/magicui/blur-in";
 import { m } from "framer-motion";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import Link from "next/dist/client/link";
 
 const sponsors = [
   {
     title: "KlickKlack",
     image:
       "https://www.kkco.com.tw/wp-content/uploads/2023/03/2023-KlickKlack-Logo.svg",
+    link: "https://www.kkco.com.tw/",
   },
   {
     title: "中華電信",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Chunghwa_Telecom.svg/2880px-Chunghwa_Telecom.svg.png",
+    link: "https://www.cht.com.tw/",
+  },
+  {
+    title: "JetBrains",
+    image:
+        "https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.svg",
+    link: "https://www.jetbrains.com/",
   },
   {
     title: "104",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/104_logo.png/1200px-104_logo.png",
-  },
-  {
-    title: "JetBrains",
-    image:
-      "https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.svg",
+    link: "https://www.104.com.tw/",
   },
 ];
 
@@ -50,19 +55,20 @@ export const Sponsors = () => {
           <BlurIn
             key={i}
             delay={0.05 * i}
-            className="rounded-md aspect-video overflow-hidden flex items-center bg-white px-4"
           >
-            <Image
-              unoptimized
-              width={720}
-              height={480}
-              key={i}
-              loading="lazy"
-              src={sponsor.image}
-              title={sponsor.title}
-              className="object-contain"
-              alt={sponsor.title}
-            />
+            <Link href={sponsor.link} rel="noreferrer noopener" className="rounded-md aspect-video overflow-hidden flex items-center bg-white px-4">
+              <Image
+                unoptimized
+                width={720}
+                height={480}
+                key={i}
+                loading="lazy"
+                src={sponsor.image}
+                title={sponsor.title}
+                className="object-contain"
+                alt={sponsor.title}
+              />
+            </Link>
           </BlurIn>
         ))}
       </div>
