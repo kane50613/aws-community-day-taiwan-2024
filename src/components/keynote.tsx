7934bd46e0a4903b7b6f80cbddf952ab366c028e
kane50613 <kane@yeecord.com>
2024-09-13 20:23:22 +0800

    typo

--- src/components/keynote.tsx
+++ src/components/keynote.tsx
@@ -23,7 +23,7 @@ export const Keynote = () => {
             AI & Machine Learning Specialist, Developer Relations@AWS
           </p>
           <p className="text-primary/85">
-            擁有電信、互聯網以及雲計算等行業超過 20¬
+            擁有電信、互聯網以及雲計算等行業超過 20
             年的豐富經驗，曾任職於微軟、Sun 和中國電信。他目前專注於生成式
             AI、大型語言模型
             (LLM)、機器學習和數據科學等領域的技術內容創作和實踐分享，致力於賦能全球開發者。
