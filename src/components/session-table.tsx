106c6dd7f81d5d3203cdaa1767c41759a4730177
kane50613 <kane@yeecord.com>
2024-09-18 21:59:47 +0800

    fix: workshop session gap

--- src/components/session-table.tsx
+++ src/components/session-table.tsx
@@ -24,7 +24,7 @@ export const SessionTable = ({
 
   if (type === "Workshop")
     return (
-      <div>
+      <div className="flex flex-col gap-8">
         {sessions.map((session, index) => (
           <WorkshopSession session={session} key={index} />
         ))}
